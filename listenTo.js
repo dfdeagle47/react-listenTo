1cfe1d950cffde08de57cf07df54d118b3e66f7d
JAlzetta <jalzetta@cognitic.be>
2015-10-28 15:43:30 +0100

    Prevent listenTo for the same (emitter, evtName, cb) multiple times

--- listenTo.js
+++ listenTo.js
@@ -1,91 +1,97 @@
 var Mixin = {
-	listenTo: function(emitter, evtName, cb) {
-		this._listeningTo = this._listeningTo || [];
+    _hasEvent: function(emitter, evtName, cb) {
+        return this._listeningTo.some(function(record) {
+            return record.emitter === emitter && record.evtName === evtName && record.cb === cb;
+        });
+    },
 
-		this._listeningTo.push({
-			cb:      cb,
-			evtName: evtName,
-			emitter: emitter
-		});
+    listenTo: function(emitter, evtName, cb) {
+        this._listeningTo = this._listeningTo || [];
 
-		bindEvent(emitter, evtName, cb);
+        //Don't listenTo if the (emitter, evtName, cb) is already there. Prevents multiple firing.
+        if (this._hasEvent(emitter, evtName, cb)) {
+            return this;
+        }
 
-		return this;
-	},
+        this._listeningTo.push({
+            cb: cb,
+            evtName: evtName,
+            emitter: emitter
+        });
 
-	noListenTo: function(emitter, evtName, cb) {
-		var listeningTo = this._listeningTo;
-		var matches = [];
+        bindEvent(emitter, evtName, cb);
 
-		if (!listeningTo || !listeningTo.length) return this;
+        return this;
+    },
 
-		if (!evtName && !cb) {
-			// stop listening to a emitter
-			matches = listeningTo.filter(function(record) {
-				return record.emitter === emitter;
-			});
-		}
-		else if (!cb) {
-			// stop listening to an event of a emitter
-			matches = listeningTo.filter(function(record) {
-				return record.emitter === emitter
-					&& record.evtName === evtName;
-			});
-		}
-		else {
-			// stop calling handler of an event of a emitter
-			matches = listeningTo.filter(function(record) {
-				return record.emitter === emitter
-					&& record.evtName === evtName
-					&& record.cb === cb
-			});
-		}
+    noListenTo: function(emitter, evtName, cb) {
+        var listeningTo = this._listeningTo;
+        var matches = [];
 
-		// unbind all matches
-		matches.forEach(function(record) {
-			unbindEvent(record.emitter, record.evtName, record.cb);
-		});
+        if (!listeningTo || !listeningTo.length) return this;
 
-		// do clean up of this._listeningTo
-		matches.forEach(function(record) {
-			var index = listeningTo.indexOf(record);
-			if (index > -1) listeningTo.splice(index, 1);
-		});
+        if (!evtName && !cb) {
+            // stop listening to a emitter
+            matches = listeningTo.filter(function(record) {
+                return record.emitter === emitter;
+            });
+        } else if (!cb) {
+            // stop listening to an event of a emitter
+            matches = listeningTo.filter(function(record) {
+                return record.emitter === emitter && record.evtName === evtName;
+            });
+        } else {
+            // stop calling handler of an event of a emitter
+            matches = listeningTo.filter(function(record) {
+                return record.emitter === emitter && record.evtName === evtName && record.cb === cb
+            });
+        }
 
-		return this;
-	},
+        // unbind all matches
+        matches.forEach(function(record) {
+            unbindEvent(record.emitter, record.evtName, record.cb);
+        });
 
-	componentWillUnmount: function() {
-		this._listeningTo.forEach(function(data) {
-			unbindEvent(data.emitter, data.evtName, data.cb);
-		});
+        // do clean up of this._listeningTo
+        matches.forEach(function(record) {
+            var index = listeningTo.indexOf(record);
+            if (index > -1) listeningTo.splice(index, 1);
+        });
 
-		return this;
-	}
+        return this;
+    },
+
+    componentWillUnmount: function() {
+        this._listeningTo.forEach(function(data) {
+            unbindEvent(data.emitter, data.evtName, data.cb);
+        });
+
+        return this;
+    }
 };
 
 function bindEvent(emitter, evtName, cb) {
-	var methods = ['on', 'addListener', 'addEventListener'];
+    var methods = ['on', 'addListener', 'addEventListener'];
 
-	// call approriate method
-	methods.some(function(method) {
-		if (typeof emitter[method] == 'function') {
-			emitter[method](evtName, cb);
-			return true;
-		}
-	});
+    // call approriate method
+    methods.some(function(method) {
+        if (typeof emitter[method] == 'function') {
+            emitter[method](evtName, cb);
+            return true;
+        }
+    });
 }
 
 function unbindEvent(emitter, evtName, cb) {
-	var methods = ['off', 'un', 'removeListener', 'removeEventListener'];
+    var methods = ['off', 'un', 'removeListener', 'removeEventListener'];
 
-	// call approriate method
-	methods.some(function(method) {
-		if (typeof emitter[method] == 'function') {
-			emitter[method](evtName, cb);
-			return true;
-		}
-	});
+    // call approriate method
+    methods.some(function(method) {
+        if (typeof emitter[method] == 'function') {
+            emitter[method](evtName, cb);
+            return true;
+        }
+    });
 }
 
 module.exports = Mixin;
